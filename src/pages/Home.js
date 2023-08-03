3df4b91bc10b52d21aba17fe0588be4f80eebf05
asom <86374869+as0mdude@users.noreply.github.com>
2023-08-02 21:56:09 -0700

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,10 @@ const Home = () => {
     return(
     <>
     
-    <h1>Home</h1>
     
-      <p>Have a prayer request? Are you burdered to pray for someone else?</p>
-      <nav>
-        <ul>
-          
-          
-        </ul>
-      </nav>
+    
+    <h1>Have a prayer request? Are you burdered to pray for someone else?</h1>
+    <h3>On this app, users are able to send prayer requests out to the database, while also being able to fetch prayer request from fellow users.</h3>
     </>
     );
   };
